0a4e8b6177f97e7f98fd7bc5c4dc0a379c7857b2
Enderness <enderledark@gmail.com>
2019-01-10 14:01:31 +0200

    Yess sir
    Copy paste working for codemirror

--- main.py
+++ main.py
@@ -4,29 +4,46 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
 import time
+import pyperclip
 
+def waitElemc(d, exa):
+
+    #Wait for element to be clickable
+    element = WebDriverWait(d, 10).until(
+    EC.element_to_be_clickable((By.XPATH, exa))
+    )
+    return element
+def waitElem(d, exa):
     
+    #Waits for element to be found
+    element = WebDriverWait(d, 10).until(
+    EC.presence_of_element_located((By.XPATH, exa))
+    )
+    return element
+
 def SanomaProLogin(username, password):
-    while (True):
-        try:
-            url = "https://kirjautuminen.sanomapro.fi/sso/XUI/#login/&realm=%2Fratkoo"
-
-            d = webdriver.Chrome("chromedriver.exe")
-
-            d.get(url)
-            d.implicitly_wait(10)
     
-            d.find_element_by_id("username").send_keys(username)
-            d.find_element_by_id("password").send_keys(password)
-            d.find_element_by_id("loginButton_0").click()
-            d.implicitly_wait(10)
+    url = "https://kirjautuminen.sanomapro.fi/sso/XUI/#login/&realm=%2Fratkoo"
 
-            d.get("https://oppimisymparisto.sanomapro.fi/d2l/le/content/custom/1545693/57176258/Viewer")
-            d.implicitly_wait(10)
-        except:
-            continue
-        break
+    d = webdriver.Chrome("chromedriver.exe")
+
+    d.get(url)
+    d.implicitly_wait(10)
+
+
+    # Saatat kysyä MIKÄ IHMEEN EXA ? No pakko venaa elementti ennenkun sen kaa voi leikkii!
+    
+    waitElem(d,'//input[@id="username"]').send_keys(username)            
+    waitElem(d,'//input[@id="password"]').send_keys(password)               
+    waitElem(d,'//input[@id="loginButton_0"]').click()
+
+    time.sleep(3)        
+    
+    d.get("https://oppimisymparisto.sanomapro.fi/d2l/le/content/custom/1545693/57176258/Viewer")
+    
+    time.sleep(3)
 
     return d
 
@@ -40,12 +57,15 @@ def saveAnswers(username, password):
 
 
 
-    #Get 4 divs and use the second
+    #Löydetään oikea div alue neljstä ->
     
     divit = []
     for x in bs.find_all("div",{"class":"course_expanded"}):
         for y in x.find_all("div",{"ng-repeat":"type in ex_types","class":"ng-scope"}):
             divit.append(y)
+    #<--
+            
+            
     with open("answers.txt","a") as fp:
         
         for y in range(1,len(divit),4):
@@ -61,17 +81,10 @@ def saveAnswers(username, password):
                 print(x['title'])
                 fp.write(x["title"])
                 fp.write("\n")
-                exa = '//span[@title="'+x["title"]+'"]'
-                element = WebDriverWait(d, 10).until(
-                EC.element_to_be_clickable((By.XPATH, exa))
-                )
-                element.click()
                 
-                exa = '//button[@ng-click="showExampleAnswer()"]'
-                element = WebDriverWait(d, 10).until(
-                EC.element_to_be_clickable((By.XPATH, exa))
-                )
-                element.click()
+                waitElem(d,'//span[@title="'+x["title"]+'"]').click()
+                
+                waitElem(d,'//button[@ng-click="showExampleAnswer()"]').click()
 
                 jatka = True
                 while (jatka):
@@ -98,7 +111,7 @@ def doExercises(username,password,valinta):
     ribs = getAnswers()
 
 
-    #Ribs on [title, koodi]
+    #Ribs on [title, koodi, title,...]
 
     
     for a in range(len(ribs)-1):
@@ -123,58 +136,53 @@ def doExercises(username,password,valinta):
             
     for y in range(1,len(divit),4):
         for x in divit[y].find_all("span",{"class":"completion-not-started"}):
-            
             if("Random" in x["title"].split(".")[0]):
                 hips = 4
             else:
                 hips = x["title"].split(".")[0]
+            
             kakat = d.find_elements_by_class_name("btn-course")
             kakat[int(hips)-1].click()
-
-            try:
-                exa = '//span[@title="'+x["title"]+'"]'
-                element = WebDriverWait(d, 10).until(
-                EC.element_to_be_clickable((By.XPATH, exa))
-                )
-                element.click()
-            except:
-                pass
+    
+            waitElemc(d,'//span[@title="'+x["title"]+'"]').click()
                 
             for a in range(0,len(ribs)-1):
                 if (x["title"] in ribs[a]):
+
+                    
                     action_chains = ActionChains(d)
+                    
                     codeMirror = WebDriverWait(d, 10).until(
                     EC.presence_of_element_located((By.CLASS_NAME, 'CodeMirror'))
                     )
                     action_chains.click(codeMirror).perform()
 
+                    koodi = ribs[a][1]
                     print(ribs[a])
-                    action_chains.send_keys(ribs[a][1].replace("\xa0","").replace("    ","")).perform()
+                    pyperclip.copy(koodi)
+        
+                    action_chains.key_down(Keys.CONTROL).send_keys("v").key_up(Keys.CONTROL).perform()
+
+                    
+                    
 
                     if(valinta == "r"):
-                        exa = '//button[contains(text(), "Run")]'
+
+                        
                         d.execute_script("window.scrollTo(0, document.body.scrollHeight);")
                     
-                        element = WebDriverWait(d, 10).until(
-                        EC.element_to_be_clickable((By.XPATH, exa))
-                        )
-                        element.click()
-                    
-                        exa = '//button[contains(text(), "Submit")]'
+                        waitElem(d,'//button[contains(text(), "Run")]').click()
+
                         d.execute_script("window.scrollTo(0, document.body.scrollHeight);")
                     
-                        element = WebDriverWait(d, 10).until(
-                        EC.element_to_be_clickable((By.XPATH, exa))
-                        )
-                        element.click()
+                        waitElem(d,'//button[contains(text(), "Submit")]').click()
+
+                        
                     else:
-                        exa = '//button[contains(text(), "Save")]'
+                        
                         d.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-                    
-                        element = WebDriverWait(d, 10).until(
-                        EC.element_to_be_clickable((By.XPATH, exa))
-                        )
-                        element.click()
+                        
+                        waitElem(d,'//button[contains(text(), "Save")]').click()
 
                     d.get("https://vw4.viope.com/student/6314/#/content")
             
@@ -188,8 +196,8 @@ def doExercises(username,password,valinta):
     
 
 def Main():
-    #doExercises("","","r")
-    #saveAnswers("","")
+    #doExercises("usr","pass","r")
+    #saveAnswers("usr","pass")
         
 
 Main()
